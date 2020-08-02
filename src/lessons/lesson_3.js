2113d035fbeda914fd38f2d34e4635fc4a83735f
Eric Martinez <eamartinez.acelas@outlook.com>
2020-08-02 16:06:51 -0500

    ejecutando funciones a partir de datos recibidos de usuarios

--- src/lessons/lesson_3.js
+++ src/lessons/lesson_3.js
@@ -1,8 +1,20 @@
-module.exports = (frist, second, personas) => {
+function f1(frist, second, personas) {
     const _ = require('lodash')
+    console.log('f1 activada')
 
     let name = _.assign(frist, second)
     let buscador = _.find(personas, { nombres: 'Michelin Mondonguis' })
     console.log(name)
     console.log(buscador)
+}
+
+module.exports = (frist, second, personas) => {
+    let comando = process.argv
+    console.log('f2 activada')
+    console.log(comando)
+    if (comando[2] === 'Eric') {
+        f1(frist,second,personas)
+    } else {
+        console.log('usuario invalido')
+    }
 }
